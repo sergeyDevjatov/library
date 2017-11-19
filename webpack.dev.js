5b7934004c206e98c7b21ac352a1312c8df2106e
Сергей Девятов <somova25@mail.ru>
2017-11-19 23:57:12 +0300

    Add dev webpack config. (without minify plugin)

---
+++ webpack.dev.js
@@ -0,0 +1,84 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        modules: [
+            "node_modules"
+        ],
+        extensions: [
+            '.js',
+            '.jsx',
+            '.css'
+        ],
+        alias: {
+            'vue': 'vue/dist/vue.esm.js',
+            'vue-router': 'vue-router/dist/vue-router.common.js'
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(scss)$/,
+                use: [{
+                    loader: 'style-loader', // inject CSS to page
+                }, {
+                    loader: 'css-loader', // translates CSS into CommonJS modules
+                }, {
+                    loader: 'postcss-loader', // Run post css actions
+                    options: {
+                        plugins: function () { // post css plugins, can be exported to postcss.config.js
+                            return [
+                                require('precss'),
+                                require('autoprefixer')
+                            ];
+                        }
+                    }
+                }, {
+                    loader: 'sass-loader' // compiles SASS to CSS
+                }]
+            },
+            {
+                test: /\.css$/,
+                loaders: [
+                    require.resolve('style-loader'),
+                    require.resolve('css-loader'),
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components|dist)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['env']
+                }
+            },
+            {
+                test: /\.js$/,
+                loader: "source-map-loader",
+                enforce: "pre"
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+                loader: 'url-loader?limit=100000'
+            }
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery',
+            Popper: ['popper.js', 'default'],
+        })
+    ]
+};
