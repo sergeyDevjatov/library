c6ac93bc5c85dff59d8ea63601d041028f8b2a13
Сергей Девятов <sergeyDevjatov@yandex.ru>
2017-11-20 11:59:27 +0300

    Realise test websocket-connection while there isn't database

--- server.js
+++ server.js
@@ -1,17 +1,39 @@
-const express = require('express')
-    , app = express()
-    , port = process.env.PORT || 80;
+const express = require('express'),
+    app = express(),
+    port = process.env.PORT || 80;
 const fs = require('fs');
+
 const root_file = 'index.html';
 
-app.get('/*', function (req, res) {
+let server = app.get('/*', function (req, res) {
     let filename = fs.existsSync(req.params['0']) ? req.params['0'] : root_file;
-    fs.readFile(filename, 'utf-8', (err, content) => {
-        res.writeHead(200, {
-            'Content-Type': 'text/html; charset=utf-8'
-        });
-        res.end(content);
-    });
+    res.sendFile(__dirname + '/' + filename);
 }).listen(port, function () {
     console.log('Listening on port ', port)
+});
+
+let io = require('socket.io')(server);
+
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+
+    socket.on('auth.sign_in', function(data){
+        if(data.login === 'root' && data.password === 'root'){
+            socket.emit('auth.sign_in-success', {sid: 'your_sid'});
+        }
+        else{
+            socket.emit('auth.sign_in-fail');
+        }
+    });
+
+    socket.on('auth.sign_out', function(data){
+        if(data.sid === 'your_sid'){
+            socket.emit('auth.sign_out-success');
+        }
+        else{
+            socket.emit('auth.sign_out-fail');
+        }
+    });
+
 });
--- src/Auth.vue
+++ src/Auth.vue
@@ -1,10 +1,79 @@
 <template>
     <div id="auth">
-        Not implemented yet.
+        <form v-if="!authorized" v-on:submit.prevent="sign_in" id="auth_form" class="form-inline">
+                <div class="form-group">
+                        <label for="auth_login" class="sr-only">Логин</label>
+                        <input type="text" class="form-control" id="auth_login" name="login" placeholder="логин" v-model="login">
+                    </div>
+                <div class="form-group mx-sm-3">
+                       <label for="auth_pass" class="sr-only">Пароль</label>
+                       <input type="password" class="form-control" id="auth_pass" name="pass" placeholder="пароль" v-model="password">
+                   </div>
+                <button type="submit" class="btn btn-primary">Войти</button>
+            </form>
+        <div class="row" v-else id="welcome">
+               <div class="col">
+                      <router-link class="link text-light" to="/personal">
+                         Добро пожаловать, {{login}}
+                     </router-link>
+             </div>
+               <div class="col col-md-auto">
+                   <button class="btn btn-primary btn-sm" v-on:click="sign_out">Выйти</button>
+               </div>
+        </div>
     </div>
 </template>
 
 <script>
+    import io from 'socket.io';
+
     export default {
-    }
+        data() {
+            return {
+                login: null,
+                password: null,
+                authorized: false,
+                sid: null,
+                socket: io()
+            }
+        },
+        beforeRouteEnter (to, from, next) {
+            let vm = this;
+            next(function(vm){
+            });
+        },
+        beforeRouteUpdate (to, from, next) {
+            let vm = this;
+            next();
+        },
+        methods: {
+            sign_in: function (){
+                this.socket.emit('auth.sign_in', {login: this.login, password: this.password});
+
+                this.socket.on('auth.sign_in-success', _.bind(function (data) {
+                    this.authorized = true;
+                    this.sid = data.sid;
+                }, this));
+
+                this.socket.on('auth.sign_in-fail', _.bind(function () {
+                    this.authorized = false;
+                }, this));
+            },
+            sign_up: function (){
+                console.log('sign up');
+            },
+            sign_out: function (){
+                this.socket.emit('auth.sign_out', {sid: this.sid});
+
+                this.socket.on('auth.sign_out-success', _.bind(function (data) {
+                    this.sid = null;
+                    this.authorized = false;
+                }, this));
+
+                this.socket.on('auth.sign_out-fail', _.bind(function () {
+                    // not implemented
+                }, this));
+            }
+        }
+    };
 </script>
