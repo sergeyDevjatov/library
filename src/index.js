223cd26f4e91a299020c67dd18b47077a5106ecb
Сергей Девятов <sergeyDevjatov@yandex.ru>
2017-11-21 01:15:04 +0300

    Include vee-validate in project

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import 'bootstrap';
 import './../style/index.scss';
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import VeeValidate from 'vee-validate';
 
+Vue.use(VeeValidate);
 Vue.use(VueRouter);
 
 import router from "./router.js";
