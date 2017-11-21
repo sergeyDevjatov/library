2ced74dbf4f0f598a3dcc84c5b09e4d72b6ac4c5
Сергей Девятов <sergey.devjatov@yandex.ru>
2017-11-22 00:18:28 +0300

    Provide sessions in sockets. Realise sign-out handler.

--- server/socket.js
+++ server/socket.js
@@ -1,53 +1,60 @@
-module.exports = function (server) {
+module.exports = function (server, sessionStore, cookieParser) {
     const user = require('./user.js');
     const _ = require('underscore');
     const crypto = require('crypto');
+    const db = require('./connect.js')();
+    const io = require('socket.io')(server);
+    const SessionSocket = require('session.socket.io');
+    const sessionIo = new SessionSocket(io, sessionStore, cookieParser);
 
-    let db = require('./connect.js')();
-    let io = require('socket.io')(server);
+    sessionIo.on('connection', function onConnect(err, socket, session) {
+        if(typeof session === 'undefined'){
+            socket.emit('connection-fail');
+            return;
+        }
+        socket.emit('session', {sid: session.sid, login: session.login});
 
-    io.on('connection', function (socket) {
+        socket.on('auth.sign_in', function onAuthSignIn(data) {
+            db.User.find({login: data.login}, function onFind(err, users) {
+                let isCorrectLoginPassPair = _.find(users, function onFind(self){
+                    return self.password === user.hash(data.password, self.salt);
+                });
 
-
-        socket.on('auth.sign_in', function (data) {
-            db.User.find({login: data.login}, function (err, users) {
-                if(_.find(users, (self) => self.password === user.hash(data.password, self.salt))){
-                    socket.sid = crypto.randomBytes(16).toString('hex');
-                    socket.emit('auth.sign_in-success', {sid: socket.sid});
-                    console.log(socket.sid);
+                if (isCorrectLoginPassPair) {
+                    session.sid = crypto.randomBytes(16).toString('hex');
+                    session.login = data.login;
+                    session.save();
+                    socket.emit('auth.sign_in-success', {sid: session.sid});
                 }
-                else{
-                    socket.emit('auth.sign_in-fail');
-                }
+                else
+                    socket.emit('auth.sign_in-fail', 'NotExists');
             });
         });
 
-        socket.on('auth.sign_up', function (data) {
-            db.User.find({login: data.login}, function (err, users) {
-                if(_.isEmpty(users)){
+        socket.on('auth.sign_up', function onAuthSignUp(data) {
+            db.User.find({login: data.login}, function onFind(err, users) {
+                if (_.isEmpty(users)) {
                     admin = new db.User(user.create(data.login, data.password));
-                    admin.save(function (err) {
+                    admin.save(function onSave(err) {
                         if (!err) {
-                            socket.sid = crypto.randomBytes(16).toString('hex');
-                            socket.emit('auth.sign_up-success', {sid: socket.sid});
+                            session.sid = crypto.randomBytes(16).toString('hex');
+                            session.login = data.login;
+                            session.save();
+                            socket.emit('auth.sign_up-success', {sid: session.sid});
                         }
-                        else{
-                            console.log(err);
-                            socket.emit('auth.sign_up-fail', err);
-                        }
+                        else
+                            socket.emit('auth.sign_up-fail');
                     });
                 }
+                else
+                    socket.emit('auth.sign_up-fail', 'AlreadyExists');
             });
         });
 
-        socket.on('auth.sign_out', function (data) {
-            if (data.sid === socket.sid) {
-                socket.sid = undefined;
-                socket.emit('auth.sign_out-success');
-            }
-            else {
-                socket.emit('auth.sign_out-fail');
-            }
+        socket.on('auth.sign_out', function onAuthSignOut(data) {
+            session.sid = session.login = null;
+            session.save();
+            socket.emit('auth.sign_out-success');
         });
 
     });
