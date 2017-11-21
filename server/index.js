341debb0baf38519c8e5099673297b53d8d2f710
Сергей Девятов <sergey.devjatov@yandex.ru>
2017-11-22 00:20:28 +0300

    Provide sessions is server. Refactory code. Now server-module returns structure

--- server/index.js
+++ server/index.js
@@ -1,3 +1,3 @@
 const path = require('path');
 let server = require('./server.js')(80, path.resolve(__dirname + '/..'));
-let io = require('./socket.js')(server);
+let io = require('./socket.js')(server.server, server.sessionStore, server.cookieParser);
--- server/server.js
+++ server/server.js
@@ -1,40 +1,58 @@
 module.exports = function(port = process.env.PORT, root) {
-    const express = require('express'),
-        app = express();
+    const express = require('express');
+    let app = express();
     const fs = require('fs');
     const path = require('path');
+    const server = require('http').Server(app).listen(port);
+    const session = require('express-session');
 
-    let index_path = path.join(root, 'index.html');
+    let SECRET = 'secret';
 
-    let server = app.get('/*', function (req, res) {
-        let req_path = path.join(root, req.params['0']);
-        let res_path = undefined;
+    let cookieParser = require('cookie-parser')(SECRET);
+    let sessionStore = new session.MemoryStore();
+
+    app.use(cookieParser);
+    app.use(session({ secret: SECRET, store: sessionStore }));
+
+    function chooseResponsePath(requestPath){
+        let INDEXPATH = path.join(root, 'index.html');
+
+        let stat;
+        try {
+            stat = fs.lstatSync(requestPath);
+        }
+        catch (err){
+            if(err.code === 'EACCES')
+                throw new Error('403');
+            else if(err.code === 'ENOENT')
+                return INDEXPATH;
+            else
+                throw err;
+        }
+        if(stat.isFile()){
+            return requestPath;
+        }
+        else if(stat.isDirectory()){
+            let requestPathWithIndex = path.join(requestPath, 'index.html');
+            if(fs.existsSync(requestPathWithIndex))
+                return requestPathWithIndex;
+            throw new Error('403');
+        }
+    }
+
+    app.get('/*', function (req, res) {
+        let reqPath = path.join(root, req.params['0']);
         try{
-            let stat = fs.lstatSync(req_path);
-            if(stat.isFile()){
-                res_path = req_path;
-            }
-            else if(stat.isDirectory()){
-                let req_path_with_index = path.join(req_path, 'index.html');
-                if(fs.existsSync(req_path_with_index))
-                    res_path = req_path_with_index;
-                else {
-                    res.status(403).send('<h1>Forbidden!</h1><h3>Access denied.</h3>');
-                    return;
-                }
-            }
-            else{
-                res_path = index_path;
-            }
+            let resPath = chooseResponsePath(reqPath);
+            res.sendFile(resPath);
         }
         catch (err) {
-            res_path = index_path;
+            if(err.message === '403')
+                res.status(403).send('<h1>Forbidden!</h1><h3>Access denied.</h3>');
+            else
+                throw err;
         }
-        res.sendFile(res_path);
-    }).
-    listen(port, function () {
-        console.log('Listening on port ', port)
     });
 
-    return server;
+    return {server, cookieParser, sessionStore};
 };
