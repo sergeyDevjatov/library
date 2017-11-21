d22f29140f35b4334d04a395da76576198aa2add
Сергей Девятов <sergey.devjatov@yandex.ru>
2017-11-22 00:16:48 +0300

    Add validation and errors processing in SignUp modal

--- src/SignUp.vue
+++ src/SignUp.vue
@@ -12,25 +12,31 @@
                     <form>
                         <div class="form-group">
                             <label for="login">Логин</label>
-                            <input type="text" class="form-control" v-model="login" id="login" placeholder="логин">
+                            <input type="text" v-model="login" id="login" placeholder="логин" name="login"
+                                   v-validate="'required'" :class="{'form-control': true, 'is-invalid': signUpError || errors.has('login')}">
                         </div>
                         <div class="form-group">
                             <label for="password">Пароль</label>
-                            <input type="password" v-validate="'confirmed:password-confirm'" name="password"
-                                   class="form-control" v-model="password" id="password" placeholder="пароль">
+                            <input type="password" v-validate="'required|confirmed:password-confirm'" name="password"
+                                   :class="{'form-control': true, 'is-invalid': signUpError || errors.has('password')}"
+                                   v-model="password" id="password" placeholder="пароль">
                         </div>
-                        <div :class="{'form-group': true, 'has-danger': errors.has('password')}">
+                        <div class="form-group">
                             <label for="password-confirm">Пароль (подтверждение)</label>
                             <input type="password" id="password-confirm" name="password-confirm"
-                                   :class="{'form-control': true, 'is-invalid': errors.has('password')}"
-                                   placeholder="пароль (подтверждение)">
+                                   :class="{'form-control': true, 'is-invalid': signUpError || errors.has('password') || errors.has('password-confirm')}"
+                                   v-validate="'required'" placeholder="пароль (подтверждение)">
                             <div v-if="errors.has('password')" class="invalid-feedback">Пароли не совпадают</div>
+                        </div>
+                        <div v-if="signUpError" class="alert alert-danger" role="alert">
+                            Не удалось зарегистрировать нового пользователя
                         </div>
                     </form>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Отмена</button>
-                    <button type="button" class="btn btn-primary" v-on:click="sign_up">Зарегистрироваться</button>
+                    <button type="button" class="btn btn-primary"
+                            v-on:click="sign_up" :disabled="!login || !password || errors.any()">Зарегистрироваться</button>
                 </div>
             </div>
         </div>
@@ -49,30 +55,33 @@
                 login: null,
                 password: null,
                 sid: null,
+                signUpError: false,
+                signUpErrorMsg: null,
                 socket: io()
             }
         },
-        beforeRouteEnter (to, from, next) {
-            let vm = this;
-            next(function(vm){
-            });
-        },
-        beforeRouteUpdate (to, from, next) {
-            let vm = this;
-            next();
+        created(){
+            this.connect();
         },
         methods: {
-            sign_up: function (){
-                this.socket.emit('auth.sign_up', {login: this.login, password: this.password});
+            connect(){
+                this.socket = io();
 
                 this.socket.on('auth.sign_up-success', _.bind(function (data) {
+                    this.error = false;
                     this.$emit('signed_up', {login: this.login, sid: data.sid});
                     $(this.$refs.modal).modal('hide');
                 }, this));
 
-                this.socket.on('auth.sign_up-fail', _.bind(function () {
-                    // TODO: if sign-up failed
+                this.socket.on('auth.sign_up-fail', _.bind(function (err) {
+                    if(err === 'AlreadyExists') {
+                        this.signUpError = true;
+                        this.signUpErrorMsg = 'Пользователь с таким логином уже существует';
+                    }
                 }, this));
+            },
+            sign_up: function (){
+                this.socket.emit('auth.sign_up', {login: this.login, password: this.password});
             },
         }
     };
