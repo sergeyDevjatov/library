9d879720258770e207a9b2911cd79f0618a99a7e
Сергей Девятов <sergey.devjatov@yandex.ru>
2017-11-22 00:21:42 +0300

    Reloading page, when session wasn't created. Provide session on client-side. Minor refactoring

--- src/Auth.vue
+++ src/Auth.vue
@@ -1,13 +1,15 @@
 <template>
-    <div id="auth">
-        <form v-if="!authorized" v-on:submit.prevent="sign_in" id="auth_form" class="form-inline">
+    <div id="auth" v-show="gotSession">
+        <form v-if="!username" v-on:submit.prevent="sign_in" id="auth_form" class="form-inline">
             <div class="form-group">
                 <label for="auth_login" class="sr-only">Логин</label>
-                <input type="text" class="form-control" id="auth_login" name="login" placeholder="логин" v-model="login">
+                <input type="text" id="auth_login" name="login" placeholder="логин" v-model="login"
+                       :class="{'form-control': true, 'is-invalid': signInError}">
             </div>
             <div class="form-group mx-sm-3">
                 <label for="auth_pass" class="sr-only">Пароль</label>
-                <input type="password" class="form-control" id="auth_pass" name="pass" placeholder="пароль" v-model="password">
+                <input type="password" id="auth_pass" name="pass" placeholder="пароль" v-model="password"
+                       :class="{'form-control': true, 'is-invalid': signInError}">
             </div>
             <button type="submit" class="btn btn-primary">Войти</button>
             <a href="#" data-toggle="modal" data-target="#sign-up_modal">Зарегистрироваться</a>
@@ -15,14 +17,14 @@
         <div class="row" v-else id="welcome">
             <div class="col">
                 <router-link class="link text-light" to="/personal">
-                    Добро пожаловать, {{login}}
+                    Добро пожаловать, {{username}}
                 </router-link>
             </div>
             <div class="col col-md-auto">
                 <button class="btn btn-primary btn-sm" @click="sign_out">Выйти</button>
             </div>
         </div>
-        <SignUp id="sign-up_modal" @signed_up="login=$event.login; authorized = true; sid = $event.sid"/>
+        <SignUp id="sign-up_modal" @signed_up="username=$event.login; sid = $event.sid"/>
     </div>
 </template>
 
@@ -38,45 +40,56 @@
             return {
                 login: null,
                 password: null,
-                authorized: false,
+                username: null,
                 sid: null,
-                show_signup: false,
-                socket: io()
+                showSignup: false,
+                socket: null,
+                signInError: false,
+                gotSession: false
             }
         },
         beforeRouteEnter (to, from, next) {
-            let vm = this;
             next(function(vm){
+                vm.connect();
             });
         },
         beforeRouteUpdate (to, from, next) {
-            let vm = this;
+            this.connect();
             next();
         },
         methods: {
-            sign_in: function (){
-                this.socket.emit('auth.sign_in', {login: this.login, password: this.password});
+            connect: function(){
+                this.socket = io();
+                this.socket.on('session', _.bind( function (data) {
+                    this.gotSession = true;
+                    this.sid = data.sid;
+                    if(this.sid) {
+                        this.username = data.login;
+                    }
+                }, this));
 
                 this.socket.on('auth.sign_in-success', _.bind(function (data) {
-                    this.authorized = true;
+                    this.username = this.login;
+                    this.login = this.password = null;
                     this.sid = data.sid;
+                    this.signInError = false;
+                    console.log(this.username);
                 }, this));
 
                 this.socket.on('auth.sign_in-fail', _.bind(function () {
-                    this.authorized = false;
+                    this.username = null;
+                    this.signInError = true;
+                }, this));
+
+                this.socket.on('auth.sign_out-success', _.bind(function () {
+                    this.sid = this.username = null;
                 }, this));
             },
+            sign_in: function (){
+                this.socket.emit('auth.sign_in', {login: this.login, password: this.password});
+            },
             sign_out: function (){
-                this.socket.emit('auth.sign_out', {sid: this.sid});
-
-                this.socket.on('auth.sign_out-success', _.bind(function (data) {
-                    this.sid = null;
-                    this.authorized = false;
-                }, this));
-
-                this.socket.on('auth.sign_out-fail', _.bind(function () {
-                    // not implemented
-                }, this));
+                this.socket.emit('auth.sign_out');
             }
         }
     };
--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import './../style/index.scss';
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import VeeValidate from 'vee-validate';
+import io from 'socket.io';
+
+let socket = io();
+
+socket.on('connection-fail', function onConnectionFail(){
+    window.location.reload();
+});
 
 Vue.use(VeeValidate);
 Vue.use(VueRouter);
