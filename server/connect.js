897f7794e249a1eeaffb711f7f31fd797b2accd7
Сергей Девятов <sergey.devjatov@yandex.ru>
2017-11-22 16:46:15 +0300

    Add rest entities.

--- server/connect.js
+++ server/connect.js
@@ -8,14 +8,33 @@ module.exports = function () {
                    mongocluster-shard-00-02-atksi.mongodb.net:27017
                    /test?ssl=true&replicaSet=MongoCluster-shard-0&authSource=admin`;
 
+
+
     mongoose.connect(mongo_url, {useMongoClient: true});
 
     mongoose.Promise = global.Promise;
 
+    userSchema = mongoose.Schema({
+        login: String, password: String, salt: String
+    });
+
+    authorSchema = mongoose.Schema({
+        name: String, born: Date, dead: Date
+    });
+
+    genreSchema = mongoose.Schema({
+        name: String
+    });
+
+    bookSchema = mongoose.Schema({
+        title: String, author: authorSchema, genre: genreSchema
+    });
+
     return {
-        User: mongoose.model('User', mongoose.Schema({
-            login: String, password: String, salt: String
-        })),
+        User: mongoose.model('User', userSchema),
+        Author: mongoose.model('Author', authorSchema),
+        Genre: mongoose.model('Genre', genreSchema),
+        Book: mongoose.model('Book', bookSchema),
         close: mongoose.connection.close
     };
 };
