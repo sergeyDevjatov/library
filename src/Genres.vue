6d2fa3400f23431d28abe8bc218ebe915b414e85
Сергей Девятов <sergey.devjatov@yandex.ru>
2017-11-27 00:30:41 +0300

    Implement the Genres component

--- src/Genres.vue
+++ src/Genres.vue
@@ -1,10 +1,61 @@
 <template>
     <div id="genres">
-        Not implemented yet.
+        <div class="text-center table-responsive table-bordered table-hover" id="books">
+            <table class="table table-striped">
+                <thead class="thead-dark">
+                <tr>
+                    <th>#</th>
+                    <th>Название</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="(genre, index) in genres">
+                    <td>{{index + 1}}</td>
+                    <td>{{genre.name}}</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
+        data(){
+            return {
+                genres: null
+            }
+        },
+        created(){
+            this.connect();
+        },
+        beforeRouteEnter (to, from, next) {
+            next(function(vm){
+            });
+        },
+        beforeRouteUpdate (to, from, next) {
+            next();
+        },
+        methods: {
+            ...Vuex.mapMutations({
+                socketOn: 'addSocketHandler',
+                socketEmit: 'emitSocketEvent'
+            }),
+            connect(){
+                this.socketOn({
+                    event: 'genres.getAll-success',
+                    callback: _.bind(function (data) {
+                        this.genres = data;
+                    }, this)
+                });
+
+                this.getAuthors();
+            },
+            getAuthors(){
+                this.socketEmit({
+                    event: 'genres.getAll'
+                });
+            }
+        }
     }
 </script>
