fe776c3bfdc8df66ceac5b28748afe7e3fc36766
Сергей Девятов <sergey.devjatov@yandex.ru>
2017-11-27 12:15:51 +0300

    Include meta in main app

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ socket.on('connection-fail', function onConnectionFail(){
 
 Vue.use(VeeValidate);
 Vue.use(VueRouter);
+Vue.use(VueMeta);
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
@@ -56,6 +57,10 @@ new Vue({
                 });
             }, this)
         });
+    },
+    metaInfo: {
+        title: 'Информационная система библиотеки',
+        titleTemplate: '%s | Информационная система библиотеки'
     },
     el: '#app'
 });
