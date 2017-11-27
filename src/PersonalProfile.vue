a6873016f1e5cd77574bdca8d69bde4b7e7dde3a
Сергей Девятов <sergey.devjatov@yandex.ru>
2017-11-27 11:45:05 +0300

    Move orders into separate component. Create component for user profile

---
+++ src/PersonalProfile.vue
@@ -0,0 +1,13 @@
+<template>
+    <div id="profile">
+        <h3>Профиль</h3>
+        <div>
+            Not implemented yet...
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+    }
+</script>
